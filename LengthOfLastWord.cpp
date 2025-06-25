bb0127781c6c41a40eaa33f1865de870ec6977a8
Ahmad <muhammadahmadamjad0@gmail.com>
2025-06-26 00:48:18 +0500

    Solved 15th Question

---
+++ LengthOfLastWord.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    int lengthOfLastWord(string s)
+    {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces at the end
+        while (i >= 0 && s[i] == ' ')
+        {
+            i--;
+        }
+
+        // Count length of the last word
+        while (i >= 0 && s[i] != ' ')
+        {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+};
