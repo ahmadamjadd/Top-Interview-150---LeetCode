8e34ad6033b5e31854306c1c7b5afa7b3c3146c3
ahmadamjadd <muhammadahmadamjad0@gmail.com>
2026-02-16 15:41:50 +0500

    Solved a question

---
+++ 97.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        i = 0
+        j = 0
+        memo = {}
+        def check(i,j):
+            if i == len(s1) and j == len(s2):
+                return True
+            if (i,j) in  memo:
+                return memo[(i,j)]
+            res = False
+            if i < len(s1) and s1[i] == s3[i+j]:
+                res = check(i + 1, j)
+
+            if not res and j < len(s2) and s2[j] == s3[i+j]:
+                res = check(i, j + 1)
+
+            memo[(i, j)] = res
+            return res
+        return check(0,0)
+
+                
+
+
+
+
+        
