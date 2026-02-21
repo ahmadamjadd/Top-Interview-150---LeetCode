b8b5d2ecd84cae6939c3644c9e31d473b53bafe1
ahmadamjadd <muhammadahmadamjad0@gmail.com>
2026-02-21 16:42:42 +0500

    Solved easy level question

---
+++ 70.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1 or n ==2:
+            return n
+        dp = [0]*(n+1)
+        dp[0] = 0
+        dp[1]= 1
+        dp[2] = 2
+        i = 3
+        while i<=n:
+            dp[i] = dp[i-1] + dp[i-2]
+            i+=1
+        return dp[-1]
+
+        
